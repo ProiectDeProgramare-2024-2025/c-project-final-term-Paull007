fcf8f584ee208dcca7d04c45e222147d8a237e0b
Paull007 <paul.rusu05@e-uvt.ro>
2025-05-28 23:35:38 +0300

    Add files via upload

--- pp.c
+++ pp.c
@@ -4,235 +4,179 @@
 #include <ctype.h>
 
 #ifdef _WIN32
-#define CLEAR_COMMAND "cls"
-#include <windows.h>
+#define CLEAR_CMD "cls"
 #else
-#define CLEAR_COMMAND "clear"
+#define CLEAR_CMD "clear"
 #endif
 
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define MAGENTA "\033[1;35m"
+#define CYAN    "\033[1;36m"
 #define RESET   "\033[0m"
 
-#define FILENAME "bikes.txt"
-#define HEADER "#GeneratedByBikeApp\n"
-
-void clear_screen() {
-    system(CLEAR_COMMAND);
-}
-
-void flush_input() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
-
-void wait_for_enter() {
-    printf("\nApasa Enter pentru a te intoarce la meniul principal...");
-    flush_input();
-    getchar();
-}
+#define MAX_RESTAURANTS 5
+#define MAX_MENU 10
+#define MAX_INGREDIENTS 10
+#define MAX_HISTORY 100
+#define ORDER_FILE "orders.txt"
 
 typedef struct {
     char name[50];
-    int total;
-    int available;
-} Bike;
+    char menu[MAX_MENU][50];
+    int menuCount;
+} Restaurant;
 
-Bike bikes[] = {
-    {"Mountain", 5, 5},
-    {"Road", 3, 3},
-    {"Electric", 4, 4}
+typedef struct {
+    char restaurant[50];
+    char orderDesc[200];
+    char location[100];
+} Order;
+
+Restaurant restaurants[MAX_RESTAURANTS] = {
+    {"Pizza Palace", {"Margherita", "Pepperoni", "Vegetarian", "BBQ Chicken"}, 4},
+    {"Burger House", {"Classic Burger", "Cheeseburger", "Bacon Burger", "Veggie Burger"}, 4},
+    {"Sushi World", {"California Roll", "Tuna Nigiri", "Salmon Sashimi", "Veggie Roll"}, 4}
 };
+int restaurantCount = 3;
 
-int bikeCount = sizeof(bikes) / sizeof(bikes[0]);
-
-void clean_line(const char *input, char *output) {
-    int j = 0;
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (isalnum(input[i]) || isspace(input[i])) {
-            output[j++] = input[i];
-        }
-    }
-    output[j] = '\0';
+void clearScreen() {
+    system(CLEAR_CMD);
 }
 
-void load_bikes() {
-    FILE *fp = fopen(FILENAME, "r");
-    if (fp != NULL) {
-        char line[256];
-        char cleaned[256];
-        if (fgets(line, sizeof(line), fp) != NULL) {
-            clean_line(line, cleaned);
-            char expectedHeader[256];
-            clean_line(HEADER, expectedHeader);
-            if (strcmp(cleaned, expectedHeader) != 0) {
-                fclose(fp);
-                return;
-            }
-        }
-        int index = 0;
-        while (index < bikeCount && fgets(line, sizeof(line), fp) != NULL) {
-            clean_line(line, cleaned);
-            char name[50];
-            int total, available;
-            if (sscanf(cleaned, "%49s %d %d", name, &total, &available) == 3) {
-                strcpy(bikes[index].name, name);
-                bikes[index].total = total;
-                bikes[index].available = available;
-            }
-            index++;
-        }
-        fclose(fp);
-    } else {
-        FILE *fpw = fopen(FILENAME, "w");
-        if (fpw != NULL) {
-            fprintf(fpw, HEADER);
-            for (int i = 0; i < bikeCount; i++) {
-                fprintf(fpw, "%s %d %d\n", bikes[i].name, bikes[i].total, bikes[i].available);
-            }
-            fclose(fpw);
-        }
-    }
+void pause() {
+    printf("\nApasa Enter pentru a continua...");
+    getchar();
 }
 
-void save_bikes() {
-    FILE *fp = fopen(FILENAME, "w");
-    if (fp != NULL) {
-        fprintf(fp, HEADER);
-        for (int i = 0; i < bikeCount; i++) {
-            fprintf(fp, "%s %d %d\n", bikes[i].name, bikes[i].total, bikes[i].available);
-        }
-        fclose(fp);
-    }
+void toLowerStr(char *s) {
+    for (int i = 0; s[i]; i++) s[i] = tolower(s[i]);
 }
 
-void afiseaza_meniu() {
-    printf("\n--- Aplicatie de Rezervare Biciclete ---\n");
-    printf("1. Rezerva o bicicleta\n");
-    printf("2. Vezi starea bicicletelor\n");
-    printf("3. Anuleaza o rezervare\n");
-    printf("4. Iesire\n");
+void initHistory() {
+    FILE *f = fopen(ORDER_FILE, "a");
+    if (f) fclose(f);
 }
 
-void rezerva_bicicleta() {
-    int opt;
-    printf("\nSelecteaza tipul de bicicleta:\n");
-    for (int i = 0; i < bikeCount; i++) {
-        printf("%d. %s%s%s (Disponibile: %s%d%s)\n", i + 1, BLUE, bikes[i].name, RESET, GREEN, bikes[i].available, RESET);
-    }
-    printf("[INPUT] Alege o optiune (1-%d): ", bikeCount);
-    if (scanf("%d", &opt) != 1 || opt < 1 || opt > bikeCount) {
-        flush_input();
-        printf(RED "[EROARE] Optiune invalida.\n" RESET);
+void saveOrder(const Order *o) {
+    FILE *f = fopen(ORDER_FILE, "a");
+    if (!f) return;
+    fprintf(f, "%s|%s|%s\n", o->restaurant, o->orderDesc, o->location);
+    fclose(f);
+}
+
+void viewHistory() {
+    clearScreen();
+    printf("--- Istoric comenzi ---\n\n");
+    FILE *f = fopen(ORDER_FILE, "r");
+    if (!f) {
+        printf("%sNu exista comenzi.%s\n", RED, RESET);
         return;
     }
-    flush_input();
-    if (bikes[opt - 1].available > 0) {
-        bikes[opt - 1].available--;
-        char nume[50];
-        printf("[INPUT] Introdu numele tau (fara spatii): ");
-        scanf("%49s", nume);
-        flush_input();
-        printf(GREEN "[SUCCES] Rezervarea pentru bicicleta '%s%s%s' a fost inregistrata pentru %s.\n" RESET,
-               BLUE, bikes[opt - 1].name, RESET, nume);
-        save_bikes();
-    } else {
-        printf(RED "[EROARE] Nu sunt disponibile biciclete de tipul %s%s%s.\n" RESET, BLUE, bikes[opt - 1].name, RESET);
+    char line[256];
+    int idx = 1;
+    while (fgets(line, sizeof(line), f)) {
+        char *r = strtok(line, "|");
+        char *d = strtok(NULL, "|");
+        char *l = strtok(NULL, "|\n");
+        printf("%d. Restaurant: %s%s%s; Comanda: %s%s%s; Locatie: %s%s%s\n",
+            idx++, CYAN, r, RESET, GREEN, d, RESET, YELLOW, l, RESET);
     }
+    fclose(f);
 }
 
-void vezi_rezervari() {
-    printf("\nStarea curenta a bicicletelor:\n");
-    for (int i = 0; i < bikeCount; i++) {
-        int rezervate = bikes[i].total - bikes[i].available;
-        printf("%s%s%s: Disponibile: %s%d%s, Rezervate: %s%d%s\n",
-               BLUE, bikes[i].name, RESET,
-               GREEN, bikes[i].available, RESET,
-               RED, rezervate, RESET);
+void customizePizza(char *desc) {
+    char size[20];
+    char ingredient[50];
+    char list[MAX_INGREDIENTS][50];
+    int count = 0;
+    printf("%sDimensiune pizza (Small/Medium/Large):%s ", YELLOW, RESET);
+    fgets(size, sizeof(size), stdin);
+    size[strcspn(size, "\n")] = '\0';
+    while (count < MAX_INGREDIENTS) {
+        printf("%sAdauga ingredient (blank pt. terminare):%s ", MAGENTA, RESET);
+        fgets(ingredient, sizeof(ingredient), stdin);
+        ingredient[strcspn(ingredient, "\n")] = '\0';
+        if (strlen(ingredient) == 0) break;
+        strcpy(list[count++], ingredient);
     }
-    printf("\nDate citite din fisierul \"%s\":\n", FILENAME);
-    FILE *fp = fopen(FILENAME, "r");
-    if (fp != NULL) {
-        char line[256];
-        char cleaned[256];
-        while (fgets(line, sizeof(line), fp) != NULL) {
-            clean_line(line, cleaned);
-            printf("%s\n", cleaned);
-        }
-        fclose(fp);
-    } else {
-        printf(RED "[EROARE] Nu se poate citi fisierul cu date.\n" RESET);
+    strcpy(desc, size);
+    strcat(desc, " pizza cu ");
+    for (int i = 0; i < count; i++) {
+        strcat(desc, list[i]);
+        if (i < count - 1) strcat(desc, ", ");
     }
+    if (count == 0) strcat(desc, "fara ingrediente");
 }
 
-void anuleaza_rezervare() {
-    int opt;
-    printf("\nSelecteaza tipul de bicicleta pentru anularea rezervarii:\n");
-    for (int i = 0; i < bikeCount; i++) {
-        int rezervate = bikes[i].total - bikes[i].available;
-        printf("%d. %s%s%s (Rezervate: %s%d%s)\n", i + 1, BLUE, bikes[i].name, RESET, RED, rezervate, RESET);
+void placeOrder() {
+    clearScreen();
+    printf("--- Plasare comanda ---\n\n");
+    for (int i = 0; i < restaurantCount; i++) {
+        printf("%d. %s%s%s\n", i + 1, CYAN, restaurants[i].name, RESET);
     }
-    printf("[INPUT] Alege o optiune (1-%d): ", bikeCount);
-    if (scanf("%d", &opt) != 1 || opt < 1 || opt > bikeCount) {
-        flush_input();
-        printf(RED "[EROARE] Optiune invalida.\n" RESET);
+    printf("%sSelecteaza restaurantul (1-%d):%s ", YELLOW, restaurantCount, RESET);
+    int choice;
+    if (scanf("%d", &choice) != 1 || choice < 1 || choice > restaurantCount) {
+        getchar();
+        printf("%sOptiune invalida.%s\n", RED, RESET);
         return;
     }
-    flush_input();
-    if (bikes[opt - 1].available < bikes[opt - 1].total) {
-        bikes[opt - 1].available++;
-        printf(GREEN "[SUCCES] Rezervarea pentru bicicleta '%s%s%s' a fost anulata.\n" RESET, BLUE, bikes[opt - 1].name, RESET);
-        save_bikes();
-    } else {
-        printf(RED "[EROARE] Nu exista rezervari pentru bicicleta %s%s%s de anulat.\n" RESET, BLUE, bikes[opt - 1].name, RESET);
+    getchar();
+    Restaurant *r = &restaurants[choice - 1];
+    printf("\nMeniu %s%s%s:\n", CYAN, r->name, RESET);
+    for (int i = 0; i < r->menuCount; i++) {
+        printf("%d. %s%s%s\n", i + 1, GREEN, r->menu[i], RESET);
     }
+    printf("%d. %sPersonalizeaza pizza%s\n", r->menuCount + 1, MAGENTA, RESET);
+    printf("%sAlege optiunea (1-%d):%s ", YELLOW, r->menuCount + 1, RESET);
+    int item;
+    if (scanf("%d", &item) != 1 || item < 1 || item > r->menuCount + 1) {
+        getchar();
+        printf("%sOptiune invalida.%s\n", RED, RESET);
+        return;
+    }
+    getchar();
+    Order o;
+    strcpy(o.restaurant, r->name);
+    if (item == r->menuCount + 1) {
+        customizePizza(o.orderDesc);
+    } else {
+        strcpy(o.orderDesc, r->menu[item - 1]);
+    }
+    printf("%s\nLocatia livrarii (ex: Strada Exemplu 12):%s ", BLUE, RESET);
+    fgets(o.location, sizeof(o.location), stdin);
+    o.location[strcspn(o.location, "\n")] = '\0';
+    if (strlen(o.location) == 0) {
+        printf("%sLocatia nu poate fi goala!%s\n", RED, RESET);
+        return;
+    }
+    saveOrder(&o);
+    printf("\n%sComanda plasata cu succes!%s\n", GREEN, RESET);
 }
 
 int main() {
-#ifdef _WIN32
-    SetConsoleOutputCP(CP_UTF8); // optional, doar daca ai caractere Unicode
-#endif
-
-    load_bikes();
-    int optiune;
+    initHistory();
     while (1) {
-        clear_screen();
-        afiseaza_meniu();
-        printf("[INPUT] Alege o optiune: ");
-        if (scanf("%d", &optiune) != 1) {
-            flush_input();
-            printf(RED "[EROARE] Optiune invalida. Te rog sa incerci din nou.\n" RESET);
-            wait_for_enter();
+        clearScreen();
+        printf("--- %sSistem Food Delivery%s ---\n", BLUE, RESET);
+        printf("1. Plasare comanda\n");
+        printf("2. Vizualizare istoric\n");
+        printf("3. Iesire\n");
+        printf("%sAlege optiunea:%s ", YELLOW, RESET);
+        int opt;
+        if (scanf("%d", &opt) != 1) {
+            getchar();
             continue;
         }
-        flush_input();
-        switch (optiune) {
-            case 1:
-                clear_screen();
-                printf("--- %sRezervare Biciclete%s ---\n", YELLOW, RESET);
-                rezerva_bicicleta();
-                wait_for_enter();
-                break;
-            case 2:
-                clear_screen();
-                printf("--- %sStare Biciclete%s ---\n", YELLOW, RESET);
-                vezi_rezervari();
-                wait_for_enter();
-                break;
-            case 3:
-                clear_screen();
-                printf("--- %sAnulare Rezervare%s ---\n", YELLOW, RESET);
-                anuleaza_rezervare();
-                wait_for_enter();
-                break;
-            case 4:
-                printf("La revedere!\n");
-                exit(0);
-            default:
-                printf(RED "[EROARE] Optiune invalida. Te rog sa incerci din nou.\n" RESET);
-                wait_for_enter();
+        getchar();
+        switch (opt) {
+            case 1: placeOrder(); pause(); break;
+            case 2: viewHistory(); pause(); break;
+            case 3: exit(0);
+            default: printf("%sOptiune invalida.%s\n", RED, RESET); pause(); break;
         }
     }
     return 0;
